5547d01da79de09f8fda59959c011f72ff377889
Back-up-Joe <jmcontrerass@gmail.com>
2025-01-28 23:24:24 -0300

    Modificación ver más-ver menos

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+document.getElementById('show-more-btn').addEventListener('click', function() {
+    var hiddenPdfs = document.getElementById('hidden-pdfs');
+    
+    // Verificar si los PDFs ocultos están visibles o no
+    if (hiddenPdfs.style.display === 'none' || hiddenPdfs.style.display === '') {
+        // Hacer visible el contenedor de PDFs ocultos
+        hiddenPdfs.style.display = 'flex';
+        // Cambiar el texto del botón a "Ver menos..."
+        this.innerText = 'Ver menos...';
+    } else {
+        // Ocultar el contenedor de PDFs ocultos
+        hiddenPdfs.style.display = 'none';
+        // Cambiar el texto del botón a "Ver más..."
+        this.innerText = 'Ver más...';
+    }
+});
+
 // Espera a que el DOM cargue
 document.addEventListener('DOMContentLoaded', () => {
     // Obtiene todos los enlaces que activan el reproductor de audio
